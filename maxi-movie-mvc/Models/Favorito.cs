a46fe6c0872c2040d57f6b90ffecb8f91e21886b
Martin Mamani <martinnnxz@gmail.com>
2026-03-20 18:34:02 -0300

    Agregado de clases de modelo

---
+++ maxi-movie-mvc/Models/Favorito.cs
@@ -0,0 +1,11 @@
+﻿namespace maxi_movie_mvc.Models
+{
+    public class Favorito
+    {
+        public int UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+        public int PeliculaId { get; set; }
+        public Pelicula? Pelicula { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
